6b847c73d4a77d3b89cda03405e20cac9b1a2570
MubashirH <hmubashir08@gmail.com>
2016-03-30 00:07:59 +0530

    Added files via upload

---
+++ mileage.py
@@ -0,0 +1,21 @@
+"""This is a doc string
+   This program calculating the mileage of a vehicle
+"""
+def get_odo_input():
+    start=float(input("Enter the start reading of odometer\n"))
+    end=float(input("Enter the end reading of odometer\n")) #the following k and l will take the inputs from the user
+    liters=float(input("Enter the amount of gas in liters\n"))
+    return float(start), float(end), float(liters)
+
+def cal_mileage(user):
+    print("The mileage of your vehicle is=%f"%a)
+      
+# main starts from here
+"""This is a doc string
+    The complete calculation of the mileage is done in main
+"""
+start,end,liters=get_odo_input()
+diff=end-start
+a=diff/liters
+cal_mileage(a)
+    
