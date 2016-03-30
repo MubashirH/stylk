da8fdced41b5a7e098f67487dd2e18e73c552297
MubashirH <hmubashir08@gmail.com>
2016-03-30 20:19:06 +0530

    Update table 17.py

--- table 17.py
+++ table 17.py
@@ -2,9 +2,9 @@
     This is a program which prints the table of 17
 """
 def table_17():
-    for i in range(0,11):
+    for i in range(1,11):
         j=17*i
-        print("%d equals 17*%d\n"%(j,i))
+        print("17 * %d = %d \n"%(i,j))
         
 #main starts from here
 table_17()
