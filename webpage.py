70ebfb935aab5e8da9196cc08d8e3960954bcac6
MubashirH <hmubashir08@gmail.com>
2016-04-21 23:06:46 +0530

    Update webpage.py

--- webpage.py
+++ webpage.py
@@ -5,41 +5,55 @@
     -at lease 2 paragraphs
 """
 
-
-def get_input():
-    head=raw_input('Enter the header for your website')
-    sub=raw_input('Enter the sub heading')
-    print 'There must and should be 2 paragraphs for your website'
-    n=int(raw_input('no of paragraphs u want to enter'))
-    if n>=2:
-        final=[]
-        i=1
-        for i in range(n):
-            par=raw_input('Enter the paragraph')
-            i+=1
-            final.append(par)
-        para='\n     </p>\n     <p>'.join(final)
-        
-    return str(head), str(sub), int(n), str(para)
-
-def webpage(head,sub,n,para):
+def title():
+    title=raw_input('Enter the title ')
+    return title
+def H1():
+    h1=raw_input('Enter the main header for your website')
+    return h1
+def H2():
+    h2=raw_input('Enter the sub header for your website')
+    return h2
+def H3():
+    h2=raw_input('Enter the topic header for your website')
+    return h2
+def P():
+    list=[]
+    i=1
+    n=int(raw_input('Enter the no of para'))
+    for i in range(n):
+        para=raw_input('Enter the paragraph')
+        i+=1
+        list.append(para)
+    return list
+def p_H1(h1):
+    print '<h1>',h1.capitalize,'</h1>'
+def p_H2(h2):
+    print '<h2>',h2.capitalize,'</h2>'
+def p_H3(h3):
+    print '<h3>',h3.capitalize,'</h3>'
+def p_head(t):
+    print ' <head>\n  <title>',t.capitalize,'</title>\n </head>'
+    
+if __name__=='__main__':
+    t=title()
+    h1=H1()
+    h2=H2()
+    h3=H3()
+    p=P()
     str='webpage.html'
     print '\n\n'
     print str.center(80,' ')
+    print '\n\n'
     print '<html>'
-    print ' <head>'
-    print '  <title>'
-    print '         ',head.capitalize()
-    print '  </title>'
-    print ' </head>'
+    p_head(t)
     print ' <body>'
-    print '     <h1>',sub.capitalize(),'</h1>'
-    print '     <p>',para,'\n     </p>'
+    print '\t',p_H1(h1)
+    print '\t<p>',p[1],'\n\t</p>'
+    print '\t',p_H2(h2)
+    print '\t<p>',p[0],'\n\t</p>'
+    print '\t',p_H3(h3)
+    print '\t<p>',p[2],'\n\t</p>'
     print ' </body>'
     print '</html>'
-             
-    
-
-#main
-head,sub,n,para=get_input()
-webpage(head,sub,n,para)
+        
