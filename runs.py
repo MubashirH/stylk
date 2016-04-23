1bc72afebce2ff9c5f2c6fdee3722a444070be1f
MubashirH <hmubashir08@gmail.com>
2016-04-24 00:01:48 +0530

    Update runs.py

--- runs.py
+++ runs.py
@@ -5,36 +5,33 @@
     -no balls wasted
     -no of boundaries and sixes
 """
-
 def get_input():
+    """get the runs per ball"""
     print "enter runs per ball"
-    name=raw_input('Enter the batsman name:')
-    i=int(raw_input('Enter total no of balls in the game:'))
-    n=1
-    runs=[]
-    for n in range (1,i+1):
-        print n,'ball'
-        run=raw_input('')
-        if run=='':
+    bats_name = raw_input('Enter the batsman name:')
+    i = int(raw_input('Enter no of balls faced by the batsman:'))
+    n_ball = 1
+    total_runs = []
+    for n_ball in range(1, i+1):
+        print n_ball, 'ball'
+        run_s = raw_input('')
+        if run_s is '':
             break
-        elif run=='.':
-            run=0
-            runs.append(run)
+        elif run_s is '.':
+            run_s = 0
+            total_runs.append(run_s)
         else:
-            ru=int(run)
-            runs.append(ru)
-        n+=1
-    return runs, name, n
-
-def cal_runs(r,ne,n):
-    print 'Total runs scored by',ne,sum(r)
-    print 'Strike rate of',ne,(sum(r)/n)
-    print 'wasted balls',r.count(0)
-    print 'boundaries',r.count(4)
-    print 'sixes',r.count(6)
-    
-        
-
-if __name__=='__main__':
-    r,ne,n=get_input()
-    cal_runs(r,ne,n)
+            ru_ns = int(run_s)
+            total_runs.append(ru_ns)
+        n_ball += 1
+    return total_runs, bats_name, n_ball
+def cal_runs(t_r, b_n, n_b):
+    """calculates the desired output"""
+    print 'Total runs scored by', b_n, sum(t_r)
+    print 'Strike rate of', b_n, (sum(t_r)/n_b)
+    print 'wasted balls', t_r.count(0)
+    print 'boundaries', t_r.count(4)
+    print 'sixes', t_r.count(6)
+if __name__ == '__main__':
+    t_r, b_n, n_b = get_input()
+    cal_runs(t_r, b_n, n_b)
