b696b1cfdef17883562c78470fa1d982f6c2dfce
MubashirH <hmubashir08@gmail.com>
2016-04-25 18:00:53 +0530

    Update beach_cricket.py

--- beach_cricket.py
+++ beach_cricket.py
@@ -5,49 +5,60 @@
 """
 def get_input():
     """takes in multiple inputs"""
-    name_1 = raw_input('name of 1st batsman:')
-    name_2 = raw_input('name of 2nd batsman:')
-    print 'write the name of batsman who is on strike and not on strike'
-    strike = raw_input('strike')
-    n_strike = raw_input('non-strike')
+    name_1 = raw_input('name of 1st batsman: ')
+    name_2 = raw_input('name of 2nd batsman: ')
+    strike_num = int(raw_input('Enter 1 if 1st batsman on strike or press 2: '))
+    if strike_num == 1:
+        strike = name_1
+        n_strike = name_2
+    else:
+        strike = name_2
+        n_strike = name_1
     return name_1, name_2, strike, n_strike
-def beach_cal():
+def beach_cal(name_1, name_2, strike, n_strike):
     """using the inputs calculates the desired output and prints the output"""
-    name_1, name_2, strike, n_strike = get_input()
     num = 1
     name_1_run = []
     name_2_run = []
     for num in range(1, 13):
         print num, 'ball'
+        if sum(name_1_run) == 15:
+            print name_1, 'has scored', sum(name_1_run), 'runs, hence they won the game'
+            break
+        elif sum(name_2_run) == 15:
+            print name_2, 'has scored', sum(name_2_run), 'runs, hence they won the game'
+            break
         run = raw_input('')
-        if run is '.':
+        if run == '':
+            print strike, 'is out and his team has lost the game'
+            break
+        if run == '.':
             run = 0
         i_run = int(run)
         if name_1 == strike:
             name_1_run.append(i_run)
         elif name_2 == strike:
             name_2_run.append(i_run)
-        if i_run % 2 is not 0:
+        if i_run % 2 != 0:
             strike, n_strike = n_strike, strike
-        if sum(name_1_run) is 60:
-            break
-        elif sum(name_2_run) is 60:
-            break
         num += 1
-        if (num-1) % 6 is 0:
+        if (num-1) % 6 == 0:
             strike, n_strike = n_strike, strike
     print name_1_run, name_2_run
-    print '*' * 10, name_1, '*' * 10
-    print 'Total runs scored:', sum(name_1_run)
-    print 'Strike rate:', float(sum(name_1_run) / len(name_1_run))
-    print 'balls wasted', name_1_run.count(0)
-    print 'boundaries', name_1_run.count(4)
-    print 'sixes', name_1_run.count(6)
-    print '*' * 10, name_2, '*' * 10
-    print 'Total runs scored:', sum(name_2_run)
-    print 'Strike rate:', float(sum(name_2_run) / len(name_2_run))
-    print 'balls wasted', name_2_run.count(0)
-    print 'boundaries', name_2_run.count(4)
-    print 'sixes', name_2_run.count(6)    
-#main
-beach_cal()
+    return name_1_run, name_2_run
+def stats(x_x, y_y):
+    """this will print all the stats of individual players"""
+    print '*' * 10, x_x, '*' * 10
+    print 'Total runs scored:', sum(y_y)
+    print 'Strike rate:', float(sum(y_y) / len(y_y))
+    print 'balls wasted', y_y.count(0)
+    print 'boundaries', y_y.count(4)
+    print 'sixes', y_y.count(6)
+def main():
+    """this is called as main function and will call all the functions"""
+    name_1, name_2, strike, n_strike = get_input()
+    name_1_run, name_2_run = beach_cal(name_1, name_2, strike, n_strike)
+    stats(name_1, name_1_run)
+    stats(name_2, name_2_run)
+if __name__ == '__main__':
+    main()
