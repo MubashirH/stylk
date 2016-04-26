e5f123a16287be6ccd1c956d9d9d6270e11c5404
MubashirH <hmubashir08@gmail.com>
2016-04-26 09:51:32 +0530

    Update beach_cricket.py

--- beach_cricket.py
+++ beach_cricket.py
@@ -5,54 +5,51 @@
 """
 def get_input():
     """takes in multiple inputs"""
-    name_1 = raw_input('name of 1st batsman: ')
-    name_2 = raw_input('name of 2nd batsman: ')
-    if name_1[-1] == '*':
-        strike = name_1[:-1]
-        n_strike = name_2
-    elif name_2[-1] == '*':
-        strike = name_2[:-1]
-        n_strike = name_1
-    else:
-        print 'please do mention who is on strike'
-        print 'now'
-        strike_num = int(raw_input('Enter 1 if 1st batsman on strike or press 2: '))
-        if strike_num == 1:
-            strike = name_1
-            n_strike = name_2
-        else:
-            strike = name_2
-            n_strike = name_1
+    name_1 = raw_input('name of batsman on strike: ')
+    name_2 = raw_input('name of batsman not on strike: ')
+    strike = name_1
+    n_strike = name_2
+    print 'on strike: ', strike
     return name_1, name_2, strike, n_strike
 def beach_cal(name_1, name_2, strike, n_strike):
     """using the inputs calculates the desired output and prints the output"""
     num = 1
     name_1_run = []
     name_2_run = []
-    for num in range(1, 13):
+    while num < 14:
         print num, 'ball'
-        if sum(name_1_run) == 15:
+        if sum(name_1_run) >= 15:
             print name_1, 'has scored', sum(name_1_run), 'runs, hence they won the game'
             break
-        elif sum(name_2_run) == 15:
+        elif sum(name_2_run) >= 15:
             print name_2, 'has scored', sum(name_2_run), 'runs, hence they won the game'
             break
-        run = raw_input('')
-        if run == '':
+        run_1 = raw_input('')
+        if run_1 == '':
             print strike, 'is out and his team has lost the game'
             break
-        if run == '.':
+        if run_1 == '.':
             run = 0
-        i_run = int(run)
-        if name_1 == strike:
-            name_1_run.append(i_run)
-        elif name_2 == strike:
-            name_2_run.append(i_run)
-        if i_run % 2 != 0:
-            strike, n_strike = n_strike, strike
-        num += 1
-        if (num-1) % 6 == 0:
-            strike, n_strike = n_strike, strike
+            if name_1 == strike:
+                name_1_run.append(run)
+            elif name_2 == strike:
+                name_2_run.append(run)
+            num += 1
+            continue
+        run_in = int(run_1)
+        if run_in < 7:
+            if name_1 == strike:
+                name_1_run.append(run_in)
+            elif name_2 == strike:
+                name_2_run.append(run_in)
+            if run_in % 2 != 0:
+                strike, n_strike = n_strike, strike
+            num += 1
+            if (num-1) % 6 == 0:
+                strike, n_strike = n_strike, strike
+        else:
+            print 'invalid input'
+            print 'try again'
     print name_1_run, name_2_run
     return name_1_run, name_2_run
 def stats(x_x, y_y):
@@ -62,7 +59,7 @@ def stats(x_x, y_y):
     if sum(y_y) != 0:
         print 'Strike rate:', float(sum(y_y) / len(y_y))
     else:
-        print 'Strike rate is: 0'
+        print 'strike rate: 0'
     print 'balls wasted', y_y.count(0)
     print 'boundaries', y_y.count(4)
     print 'sixes', y_y.count(6)
