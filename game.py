ac544ebc1d89722c0ce72eca1575ec7640b587d5
MubashirH <hmubashir08@gmail.com>
2016-06-26 13:08:48 +0530

    Create game.py

---
+++ game.py
@@ -0,0 +1,45 @@
+"""program is a luck game simulating the following
+    Asking 10 players to pick card btw 300-325
+    The player with maximum number wins
+"""
+import random
+from arrange import arrange
+def get_input():
+    """takes the names of the players"""
+    players = int(raw_input('Enter no of players'))
+    ply = players + 1
+    i = 0
+    names = []
+    for i in range(1,ply):
+        name = raw_input('Enter your name')
+        if name == '':
+            print '----------Please enter a name-----------'
+            name = raw_input('Enter the name')
+        names.append(name)
+    return names, players
+def game(nam, rng):
+    """Algorithm of the game"""
+    nums = random.sample(range(300,325),rng)
+    i = 0
+    while i < rng:
+        print nam[i], '=', nums[i]
+        i += 1
+    grt = []
+    for i  in range(rng):
+        grt.append(nums[i])
+    arrange(grt, rng)
+    for i in range(rng):
+        if nums[i] != grt[rng-1]:
+            continue
+        else:
+            print nam[i], 'has won the game'
+            break
+def main():
+    """This is the main"""
+    nam, rng = get_input()
+    game(nam, rng)
+if __name__ == '__main__':
+    main()
+    
+
+
