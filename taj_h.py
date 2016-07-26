7f5471312b7cd5dc672996ff40b4b5395ec3e841
MubashirH <hmubashir08@gmail.com>
2016-07-26 21:10:38 +0530

    Update taj_h.py

--- taj_h.py
+++ taj_h.py
@@ -1,19 +1,23 @@
 """Program to calculate the following
     -Height of the Taj tower
     -Distance from the Taj gate to the gateway of india
-    Given angle of elevation from
-    Gateway of India = 25
-    Taj hotel gate = 65
+    Angle of elivations are taken as input
 """
 import math
-def calculation():
+def get_input():
+    """take the elivations from different view points"""
+    ang_gt = float(raw_input('Elivation angle from gateway of india to top of taj tower:'))
+    ang_taj = float(raw_input('Elivation angle from Taj gate to top of taj tower:'))
+    return ang_gt, ang_taj
+def calculation(ang_g_t, ang_ta_j):
     """Algorithm to calculate the height n distance"""
-    h_of_twr = round(math.tan(math.radians(65)) * 100)
-    dis_to_gat = round((h_of_twr / math.tan(math.radians(25))) - 100)
+    h_of_twr = round(math.tan(math.radians(ang_ta_j)) * 100)
+    dis_to_gat = round((h_of_twr / math.tan(math.radians(ang_g_t))) - 100)
     print h_of_twr, 'yards is the height of the tower'
     print dis_to_gat, 'yards is the distace from Taj gate to the Gateway of India'
 def main():
     """this is main"""
-    calculation()
+    ang_g_t, ang_ta_j = get_input()
+    calculation(ang_g_t, ang_ta_j)
 if __name__ == '__main__':
     main()
